56ff540014d8131365f2d13b2a25f127459a7600
SimoneLuchini <88744446+SimoneLuchini@users.noreply.github.com>
2023-04-09 02:00:28 -0400

    Add files via upload
    LSTM and pdf

---
+++ LSTM_nlpHomework2.py
@@ -0,0 +1,361 @@
+###########################################################################################################################################
+import pandas as pd
+
+INPUT_FOLDER = '/Users/simone/desktop/Homework2_NLP/LSTM_task'
+OUTPUT_FOLDER = '/Users/simone/desktop/Homework2_NLP/LSTM_task'
+
+top_data_df = pd.read_csv(INPUT_FOLDER + '/output_reviews_top.csv')
+print("Columns in the original dataset:\n")
+print(top_data_df.columns)
+
+
+
+################################################################################################################################
+#defining true sentiment from stars
+
+import matplotlib.pyplot as plt 
+
+#print count of rows per star rating
+print("Number of rows per star rating:")
+print(top_data_df['stars'].value_counts())
+
+# Function to map stars to sentiment
+def map_sentiment(stars_received):
+    if stars_received <= 2:
+        return -1
+    elif stars_received == 3:
+        return 0
+    else:
+        return 1
+
+# Mapping stars to sentiment into three categories
+top_data_df['sentiment'] = [ map_sentiment(x) for x in top_data_df['stars']]
+
+
+# Plotting the sentiment distribution
+plt.figure()
+pd.value_counts(top_data_df['sentiment']).plot.bar(title="Sentiment distribution in df")
+plt.xlabel("Sentiment")
+plt.ylabel("No. of rows in df")
+plt.show()
+
+
+###################################################################################################################################################
+##Creating equal distribution of each sentiment in df
+# Function to retrieve top few number of each category
+def get_top_data(top_n = 5000):
+    top_data_df_positive = top_data_df[top_data_df['sentiment'] == 1].head(top_n)
+    top_data_df_negative = top_data_df[top_data_df['sentiment'] == -1].head(top_n)
+    top_data_df_neutral = top_data_df[top_data_df['sentiment'] == 0].head(top_n)
+    top_data_df_small = pd.concat([top_data_df_positive, top_data_df_negative, top_data_df_neutral])
+    return top_data_df_small
+
+# Function call to get the top 10000 from each sentiment
+top_data_df_small = get_top_data(top_n=10000)
+
+# After selecting top few samples of each sentiment
+print("After segregating and taking equal number of rows for each sentiment:")
+print(top_data_df_small['sentiment'].value_counts())
+top_data_df_small.head(10)
+
+
+# Plotting the sentiment distribution
+plt.figure()
+pd.value_counts(top_data_df_small['sentiment']).plot.bar(title="Sentiment distribution in df")
+plt.xlabel("Sentiment")
+plt.ylabel("No. of rows in df")
+plt.show()
+
+
+
+
+
+########################################################################################################################
+#PREPROCESS
+from gensim.parsing.preprocessing import remove_stopwords
+
+# Define a function to remove stopwords from a sentence
+def preprocess_sentence(sentence):
+    return remove_stopwords(sentence)
+
+# Apply the preprocessing function to the Sentences column
+top_data_df_small['ProcessedSentences'] = top_data_df_small['text'].apply(preprocess_sentence)
+
+
+from gensim.utils import simple_preprocess
+
+# Tokenize the text column to get the new column 'tokenized_text'
+top_data_df_small['tokenized_text'] = [simple_preprocess(line, deacc=True) for line in top_data_df_small['ProcessedSentences']] 
+print(top_data_df_small['tokenized_text'].head(10))
+
+from gensim.parsing.porter import PorterStemmer
+
+porter_stemmer = PorterStemmer()
+# Get the stemmed_tokens
+top_data_df_small['stemmed_tokens'] = [[porter_stemmer.stem(word) for word in tokens] for tokens in top_data_df_small['tokenized_text'] ]
+top_data_df_small['stemmed_tokens'].head(10)
+
+
+
+
+###################################################################################################################################
+##### TRAIN TEST SPLIT
+
+from sklearn.model_selection import train_test_split
+
+# Train Test Split Function
+def split_train_test(top_data_df_small, test_size=0.2, shuffle_state=True):
+    X_train, X_test, Y_train, Y_test = train_test_split(top_data_df_small[['business_id', 'cool', 'date', 'funny', 'review_id', 'stars', 'text', 'useful', 'user_id', 'stemmed_tokens']], 
+                                                        top_data_df_small['sentiment'], 
+                                                        shuffle=shuffle_state,
+                                                        test_size=test_size, 
+                                                        random_state=15)
+    print("Value counts for Train sentiments")
+    print(Y_train.value_counts())
+    print("Value counts for Test sentiments")
+    print(Y_test.value_counts())
+    print(type(X_train))
+    print(type(Y_train))
+    X_train = X_train.reset_index()
+    X_test = X_test.reset_index()
+    Y_train = Y_train.to_frame()
+    Y_train = Y_train.reset_index()
+    Y_test = Y_test.to_frame()
+    Y_test = Y_test.reset_index()
+    print(X_train.head())
+    return X_train, X_test, Y_train, Y_test
+
+# Call the train_test_split
+X_train, X_test, Y_train, Y_test = split_train_test(top_data_df_small)
+
+
+
+
+########################################################################################################################
+#Packages for LSTM
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+###################################################################################################################
+#WORD2VEC
+from gensim.models import Word2Vec
+
+w2vmodel = Word2Vec.load(OUTPUT_FOLDER + '/models/' + 'word2vec_500_PAD.model')
+
+max_sen_len = top_data_df_small.stemmed_tokens.map(len).max()
+padding_idx = w2vmodel.wv.key_to_index['pad']
+def make_word2vec_vector_lstm(sentence):
+    padded_X = [padding_idx for i in range(max_sen_len)]
+    i = 0
+    for word in sentence:
+        if word not in w2vmodel.wv.key_to_index:
+            padded_X[i] = 0
+            print(word)
+        else:
+            padded_X[i] = w2vmodel.wv.key_to_index[word]
+        i += 1
+    return torch.tensor(padded_X, dtype=torch.long, device=device).view(1, -1)
+
+def make_target(label):
+    if label == -1:
+        return torch.tensor([0], dtype=torch.long, device=device)
+    elif label == 0:
+        return torch.tensor([1], dtype=torch.long, device=device)
+    else:
+        return torch.tensor([2], dtype=torch.long, device=device)
+
+
+
+
+##############################################################################################################
+##########Define LSTM
+
+EMBEDDING_SIZE = 500
+NUM_FILTERS = 10
+import gensim
+
+class LSTMTextClassifier(nn.Module):
+    def __init__(self, vocab_size, num_classes, hidden_size=128, num_layers=2, bidirectional=True):
+        super(LSTMTextClassifier, self).__init__()
+        self.embedding_size = 500
+        w2vmodel = gensim.models.Word2Vec.load(OUTPUT_FOLDER + '/models/' + 'word2vec_500_PAD.model')
+        weights = w2vmodel.wv
+        self.embedding = nn.Embedding.from_pretrained(torch.FloatTensor(weights.vectors), padding_idx=weights.key_to_index['pad'])
+        self.lstm = nn.LSTM(input_size=self.embedding_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            bidirectional=bidirectional,
+                            batch_first=True)
+        self.fc = nn.Linear(hidden_size * 2 if bidirectional else hidden_size, num_classes)
+
+    def forward(self, x):
+        x = self.embedding(x) # [B, T, E]
+        out, _ = self.lstm(x) # [B, T, H]
+        out = torch.cat((out[:, -1, :self.lstm.hidden_size], out[:, 0, self.lstm.hidden_size:]), dim=1) if self.lstm.bidirectional else out[:, -1, :]
+        logits = self.fc(out)
+        probs = F.softmax(logits, dim=1)
+        return probs
+
+
+
+
+######################################################################################################
+########TRAIN LSTM 
+# Set the parameters
+NUM_CLASSES = 3
+VOCAB_SIZE = len(w2vmodel.wv.index_to_key)
+
+lstm_model = LSTMTextClassifier(vocab_size=VOCAB_SIZE, num_classes=NUM_CLASSES)
+lstm_model.to(device)
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(lstm_model.parameters(), lr=0.001)
+num_epochs = 3
+padding_idx = 0
+
+# Open the file for writing loss
+loss_file_name = OUTPUT_FOLDER + '/plots/' + 'lstm_class_big_loss_with_padding.csv'
+f = open(loss_file_name, 'w')
+f.write('iter, loss')
+f.write('\n')
+losses = []
+lstm_model.train()
+
+# Train Model
+from tqdm import tqdm
+
+# Loop over epochs
+for epoch in range(num_epochs):
+    print("Epoch" + str(epoch + 1))
+
+    # Initialize progress bar
+    progress_bar = tqdm(X_train.iterrows(), total=len(X_train))
+
+    # Initialize training loss
+    train_loss = 0
+
+    # Loop over training examples
+    for index, row in progress_bar:
+        # Clearing the accumulated gradients
+        lstm_model.zero_grad()
+
+        # Make the sequence of word embeddings for stemmed tokens
+        word_embeddings = make_word2vec_vector_lstm(row['stemmed_tokens'])
+
+        # Forward pass to get output
+        probs = lstm_model(word_embeddings)
+
+        # Get the target label
+        target = make_target(Y_train['sentiment'][index])
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = loss_function(probs, target)
+        train_loss += loss.item()
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+        # Update progress bar
+        progress_bar.set_description(f'Training loss: {train_loss / (index+1):.4f}')
+        progress_bar.update(1)
+
+    # Finish the progress bar for the epoch
+    progress_bar.close()
+
+    # Write results to file
+    f.write(str((epoch + 1)) + "," + str(train_loss / len(X_train)))
+    f.write('\n')
+
+    # Reset training loss
+    train_loss = 0
+
+
+
+torch.save(lstm_model, OUTPUT_FOLDER + '/lstm_big_model_500_with_padding.pth')
+
+f.close()
+print("Input vector")
+print(bow_vec.cpu().numpy())
+print("Probs")
+print(probs)
+print(torch.argmax(probs, dim=1).cpu().numpy()[0])
+
+
+
+############################################################################################################################################################
+#TEST MODEL
+
+from sklearn.metrics import classification_report
+bow_lstm_predictions = []
+original_lables_lstm_bow = []
+lstm_model.eval()
+loss_df = pd.read_csv(OUTPUT_FOLDER + '/lstm_class_big_loss_with_padding.csv')
+print(loss_df.columns)
+# loss_df.plot('loss')
+
+with torch.no_grad():
+    for index, row in X_test.iterrows():
+        bow_vec = make_word2vec_vector_lstm(row['stemmed_tokens'])
+        probs = lstm_model(bow_vec)
+        _, predicted = torch.max(probs.data, 1)
+        bow_lstm_predictions.append(predicted.cpu().numpy()[0])
+        original_lables_lstm_bow.append(make_target(Y_test['sentiment'][index]).cpu().numpy()[0])
+
+print(classification_report(original_lables_lstm_bow,bow_lstm_predictions))
+
+loss_file_name = OUTPUT_FOLDER +  '/plots/' + 'cnn_class_big_loss_with_padding.csv'
+loss_df = pd.read_csv(loss_file_name)
+print(loss_df.columns)
+plt_500_padding_30_epochs = loss_df[' loss'].plot()
+fig = plt_500_padding_30_epochs.get_figure()
+fig.savefig(OUTPUT_FOLDER +'/plots/' + 'loss_plt_500_padding_30_epochs.pdf')
+
+
+
+# Convert report to pandas dataframe
+report = classification_report(original_lables_lstm_bow,bow_lstm_predictions, output_dict=True)
+df = pd.DataFrame(report)
+
+# Transpose the dataframe so that classes are columns and metrics are rows
+df = df.transpose()
+
+# Save dataframe as csv
+df.to_csv(OUTPUT_FOLDER + '/classification_report.csv', index=True)
+
+
+
+
+
+##########
+#check hidden layer
+lstm_model = torch.load(OUTPUT_FOLDER + '/lstm_big_model_500_with_padding.pth', map_location=torch.device('cpu'))
+
+embedding_params = sum(p.numel() for p in lstm_model.embedding.parameters())
+print(f"Number of parameters in the embedding layer: {embedding_params}")
+
+def print_num_params(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'Total number of parameters: {total_params:,}\n')
+
+    for name, param in model.named_parameters():
+        num_params = param.numel()
+        print(f'{name}: {num_params:,}')
+
+print_num_params(lstm_model)
+
+
+# define input sequence
+input_sequence = torch.tensor([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+
+print(output.shape)
+output
+
+# print hidden state tensor
+print(hidden_state)
+
